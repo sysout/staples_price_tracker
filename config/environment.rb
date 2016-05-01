b1e9bfa6786fdce0f6cbdb1067eb1428724cac33
Xiong Xiong <xiong_xiong@comcast.com>
2016-05-01 16:06:46 -0700

    Comment out CarrierWave

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
 # Load the Rails application.
-# TODO Xiong
-# require File.expand_path('../application', __FILE__)
+require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
