b2fc7095a7a8fc8c981d02008549a150b0bc1a4b
Xiong Xiong <xiong_xiong@comcast.com>
2016-05-05 23:49:59 -0700

    Refactor product.rb, cache the @page object, add link to images

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,28 +5,41 @@ class Product < ActiveRecord::Base
   validate :product_exists
   enum availability: [:instock, :oos]
 
-  def get_page(p_id)
+  has_many :alerts
+
+  def unavailable?(title)
+    !title or title.match("Unavailable")
+  end
+
+
+  def load_page(reload=false)
+    return @page if @page and reload==false
     agent = Mechanize.new
     agent.user_agent_alias = 'Mac Safari'
     agent.history_added = Proc.new { sleep 0.5 }
-    agent.get("http://www.staples.com/product_#{p_id}")
+    page=agent.get("http://www.staples.com/product_#{self.staples_pid}")
+    return page if unavailable?(page.title)
+    @page=page
+  end
+
+  def get_price(page)
+    page.search('span[itemprop="price"]').text.sub("$", "").to_f
+  end
+
+  def current_price
+    get_price load_page
   end
 
   def load_product
-    if self.id!=nil
-      # product exist
-      return
-    end
-    page=get_page(self.staples_pid)
+    return if self.id!=nil # product exist
+    page=load_page
     self.name=page.title
-    if self.name==nil or self.name.match("Unavailable")
-      return
-    end
+    return if unavailable?(self.name)
     self.name=page.search('[itemprop="name"]').text
     self.price=page.search('span[itemprop="price"]').text.sub("$", "").to_f
     self.description=page.search('div[itemprop="description"]').text
-    self.image_url=page.search('img[itemprop="image"]').attr('src').text.sub(/\Ahttp:/,"")
-    self.url=page.search('a[itemprop="url"]').attr('href').text.sub(/\Ahttp:/,"")
+    self.image_url=page.search('img[itemprop="image"]').attr('src').text.sub(/\Ahttp:/, "")
+    self.url=page.search('a[itemprop="url"]').attr('href').text.sub(/\Ahttp:/, "")
     # ignore item condition, user should know more
     # itemCondition=page.search('link[itemprop="itemCondition"]').attr('href').text
     if "http://schema.org/InStock"==page.search('link[itemprop="availability"]').attr('href').text
@@ -46,4 +59,5 @@ class Product < ActiveRecord::Base
     end
   end
 
+  private :unavailable?
 end
