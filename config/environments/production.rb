1e9a6192aa75d6206eb1178e87ac96abacf7e5db
Xiong Xiong <xiong_xiong@comcast.com>
2016-05-07 19:06:28 -0700

    Add ENV['APP_HOST_NAME'] to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'fathomless-journey-48002.herokuapp.com'
+  host = ENV['APP_HOST_NAME']
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
