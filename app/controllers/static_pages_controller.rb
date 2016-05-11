9d5c6c9c8cc08133bf6d07843430f015a960e4c4
Xiong Xiong <xiong_xiong@comcast.com>
2016-05-11 18:52:13 -0400

    Add pagination

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   
   def home
-    @products = Product.where('alerts_count > 0').order(alerts_count: :desc)
+    @products = Product.where('alerts_count > 0').order(alerts_count: :desc).paginate(page: params[:page], :per_page => 10)
   end
 
   def help
