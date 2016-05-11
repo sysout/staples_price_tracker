372d3a8793898ba0b86259bc1891d6064df0a7d0
Xiong Xiong <xiong_xiong@comcast.com>
2016-05-11 18:41:15 -0400

    Add 'My Alerts' to header. Remove login check from home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,6 @@
 class StaticPagesController < ApplicationController
   
   def home
-    if logged_in?
-      redirect_to alerts_path
-    end
     @products = Product.where('alerts_count > 0').order(alerts_count: :desc)
   end
 
