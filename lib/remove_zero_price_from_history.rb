29b7167d30e76d6a12ba68737a5743e608ebd2ae
Xiong Xiong <xiong_xiong@comcast.com>
2016-05-24 21:10:09 -0700

    Remove dup from price history

--- lib/remove_zero_price_from_history.rb
+++ lib/remove_zero_price_from_history.rb
@@ -4,3 +4,15 @@ Product.all.each do |p|
     p.price_histories.create(price: p.price, updated_at: p.updated_at, created_at: p.updated_at)
   end
 end
+Product.all.each do |p|
+  dup=[]
+  for i in 1...p.price_histories.length
+    if p.price_histories[i].price==p.price_histories[i-1].price
+      dup<<p.price_histories[i-1]
+    end
+  end
+  if !dup.empty?
+    puts dup.map {|ph| "#{ph.product.name} #{ph.price}"}.join(", ")
+    dup.each{|ph| ph.destroy }
+  end
+end
